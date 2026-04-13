08017ebf19fa0e96a4c4ab57ba70b3501f90a197
Saurabh Sharma <275472447+Saurabh12-12@users.noreply.github.com>
2026-04-13 16:56:23 +0530

    deleted

--- build_exe.bat
+++
@@ -1,7 +0,0 @@
-```batch
-@echo off
-echo Building Taskify EXE...
-pip install pyinstaller
-pyinstaller --onefile --windowed --icon=icon.ico --name "Taskify" Taskify_app.py
-echo Done! Check dist/Taskify.exe
-pause
